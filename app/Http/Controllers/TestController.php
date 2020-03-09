7a897a9c8ac31be885c1b93e683e6ab5bc292fcc
liu <1807578838@qq.com>
2020-03-09 03:32:05 +0000

    redis

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    /**
+     * redis
+     */
+    public function redis1()
+    {
+        echo 11;
+    }
+    public function redis2()
+    {
+        echo 22;
+    }
+}
